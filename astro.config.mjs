2c02308dead4177742183b558916755c0317144d
Aryan-IIT <illogicalaryan@iitgn.ac.in>
2025-10-31 01:22:31 +0530

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
+// import tailwind from "@astrojs/tailwind";
+
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   base: '/',
+//   // If you ever host in a subfolder like /portfolio/, you’d write base: '/portfolio/'
+// });
+
+// // https://astro.build/config
+// // export default defineConfig({
+// //   integrations: [tailwind()]
+// // });
+
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://aryan-iit.github.io',
+  base: '/',
+  integrations: [tailwind()],
 });
